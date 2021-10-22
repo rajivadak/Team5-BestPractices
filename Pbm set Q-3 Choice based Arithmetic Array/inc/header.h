345be434c745dd0ced1b6dbee2ebbb432c7a8f37
shrishti-1309 <69230664+shrishti-1309@users.noreply.github.com>
2021-10-22 16:38:32 +0530

    Update header.h

--- Pbm set Q-3 Choice based Arithmetic Array/inc/header.h
+++ Pbm set Q-3 Choice based Arithmetic Array/inc/header.h
@@ -1,1 +1,14 @@
 
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+#include <stdio.h>
+
+typedef int (*arithFuncPtr)(int, int);
+
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
+
+#endif 
